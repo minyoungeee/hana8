d42e3f8d86b81275d0b6065f7fb196f4389dcd8f
zerouriban <minyoung6048@gmail.com>
2025-12-15 20:25:51 +0900

    react add- useReducer example

--- react/rbvite/src/hooks/SessionContext.tsx
+++ react/rbvite/src/hooks/SessionContext.tsx
@@ -1,8 +1,8 @@
 import {
   createContext,
   use,
+  useReducer,
   useRef,
-  useState,
   type PropsWithChildren,
   type RefObject,
 } from 'react';
@@ -54,54 +54,148 @@ const SessionContext = createContext<SessionContextValue>({
   editItem: () => {},
 });
 
+type Action =
+  | { type: 'login'; payload: LoginUser }
+  | { type: 'logout' }
+  | { type: 'addItem'; payload: ItemType }
+  | { type: 'removeItem'; payload: number }
+  | { type: 'editItem'; payload: { id: number; name: string; price: number } };
+
+const reducer = (session: Session, action: Action): Session => {
+  switch (action.type) {
+    case 'login':
+      return {
+        ...session,
+        loginUser: action.payload,
+      };
+
+    case 'logout':
+      return {
+        ...session,
+        loginUser: null,
+        cart: [],
+      };
+
+    case 'addItem':
+      return {
+        ...session,
+        cart: [...session.cart, action.payload],
+      };
+
+    case 'removeItem':
+      return {
+        ...session,
+        cart: session.cart.filter((item) => item.id !== action.payload),
+      };
+
+    case 'editItem':
+      return {
+        ...session,
+        cart: session.cart.map((item) =>
+          item.id === action.payload.id
+            ? {
+                ...item,
+                name: action.payload.name,
+                price: action.payload.price,
+              }
+            : item
+        ),
+      };
+
+    default:
+      return session;
+  }
+};
+
 // 2. Provider
 export function SessionProvider({ children }: PropsWithChildren) {
-  const [session, setSession] = useState<Session>(DefaultSession);
+  // const [session, setSession] = useState<Session>(DefaultSession);
 
   const loginHandlerRef = useRef<LoginHandler>(null);
 
-  const logout = () => {
-    // session.loginUser = null; //fail
-    setSession({ ...session, loginUser: null });
-  };
+  // TODO 기본 방식
+
+  // const logout = () => {
+  //   // session.loginUser = null; //fail
+  //   setSession({ ...session, loginUser: null });
+  // };
+
+  // const login: LoginFunction = (name, age) => {
+  //   if (loginHandlerRef.current?.validate())
+  //     setSession({ ...session, loginUser: { id: 1, name, age } });
+  // };
+
+  // const removeItem = (id: number) => {
+  //   if (!confirm('Are u sure?')) return;
+
+  //   // 이 코드는 보기 좋지 않음! !
+  //   // setSession({...session, cart: [...session.cart.filter((item) => item.id !== id)]});
+  //   //TODO 이 코드가 맞는 것임!
+  //   setSession({
+  //     ...session,
+  //     cart: session.cart.filter((item) => item.id !== id),
+  //   });
+  // };
+
+  // const addItem = (name: string, price: number) => {
+  //   const newItem = {
+  //     id: Math.max(...session.cart.map((item) => item.id), 0) + 1, //TODO Math.max는 이더레이터로 받아야 함
+  //     name,
+  //     price,
+  //   };
+  //   //TODO 이 코드는 좋지 않음 : 반드시 재 렌더링되기 때문에
+  //   // session.cart.push(newItem)
+  //   // setSession({...session})
+  //   //TODO 이 코드로 사용해야함
+  //   setSession({ ...session, cart: [...session.cart, newItem] });
+  // };
+
+  // const editItem = (id: number, name: string, price: number) => {
+  //   setSession((prev) => ({
+  //     ...prev,
+  //     cart: prev.cart.map((item) =>
+  //       item.id === id ? { ...item, name, price } : item
+  //     ),
+  //   }));
+  // };
+
+  // TODO : reduce 이용
+
+  const [session, dispatch] = useReducer(reducer, DefaultSession);
 
   const login: LoginFunction = (name, age) => {
-    if (loginHandlerRef.current?.validate())
-      setSession({ ...session, loginUser: { id: 1, name, age } });
+    if (!loginHandlerRef.current?.validate()) return;
+
+    dispatch({
+      type: 'login',
+      payload: { id: 1, name, age },
+    });
+  };
+
+  const logout = () => {
+    dispatch({ type: 'logout' });
+  };
+
+  const addItem = (name: string, price: number) => {
+    const newItem: ItemType = {
+      id: Math.max(...session.cart.map((item) => item.id), 0) + 1,
+      name,
+      price,
+    };
+
+    dispatch({ type: 'addItem', payload: newItem });
   };
 
   const removeItem = (id: number) => {
     if (!confirm('Are u sure?')) return;
-
-    // 이 코드는 보기 좋지 않음! !
-    // setSession({...session, cart: [...session.cart.filter((item) => item.id !== id)]});
-    //TODO 이 코드가 맞는 것임!
-    setSession({
-      ...session,
-      cart: session.cart.filter((item) => item.id !== id),
-    });
-  };
-
-  const addItem = (name: string, price: number) => {
-    const newItem = {
-      id: Math.max(...session.cart.map((item) => item.id), 0) + 1, //TODO Math.max는 이더레이터로 받아야 함
-      name,
-      price,
-    };
-    //TODO 이 코드는 좋지 않음 : 반드시 재 렌더링되기 때문에
-    // session.cart.push(newItem)
-    // setSession({...session})
-    //TODO 이 코드로 사용해야함
-    setSession({ ...session, cart: [...session.cart, newItem] });
+    dispatch({ type: 'removeItem', payload: id });
   };
 
   const editItem = (id: number, name: string, price: number) => {
-    setSession((prev) => ({
-      ...prev,
-      cart: prev.cart.map((item) =>
-        item.id === id ? { ...item, name, price } : item
-      ),
-    }));
+    dispatch({
+      type: 'editItem',
+      payload: { id, name, price },
+    });
   };
 
   return (
